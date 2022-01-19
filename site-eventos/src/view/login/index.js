871d16f5646d85f0a77aada9cdd4ef6cd9913af8
João Hélio <85315750+JoaoSantosJHX@users.noreply.github.com>
2022-01-19 10:04:13 -0300

    font weight fixed

--- site-eventos/src/view/login/index.js
+++ site-eventos/src/view/login/index.js
@@ -6,23 +6,22 @@ function Login() {
     <div className="login-content d-flex align-items-center">
         <form className="form-signin mx-auto">
             <img className="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="Login" width="72" height="57"/>
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <h1 class="h3 mb-3 fw-bold text-white font-weight-bold text-center">Login</h1>
 
-            <div className="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+            {/* <div className="form-floating">
+            <input type="email" class="form-control my-2" id="floatingInput" placeholder="name@example.com"/>
             <label for="floatingInput">Email address</label>
-            </div>
-            <div className="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-            <label for="floatingPassword">Password</label>
-            </div>
+            </div> */}
+            <input type="email" id="inputEmail" class="form-control my-2" placeholder="email"/>
+
+            <input type="password" id="inputPassword" class="form-control my-2" placeholder="senha"/>
 
             <div className="checkbox mb-3">
             <label>
                 <input type="checkbox" value="remember-me"/> Remember me
             </label>
             </div>
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <button className="w-100 btn btn-lg btn-login" type="submit">Sign in</button>
             <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
         </form>
     </div>
