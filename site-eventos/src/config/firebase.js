faa917d8f6c960e30bde6d5dca3b39fdba8b6a81
João Hélio <85315750+JoaoSantosJHX@users.noreply.github.com>
2022-01-23 10:00:13 -0300

    firebase login autho finalized successfully

---
+++ site-eventos/src/config/firebase.js
@@ -0,0 +1,25 @@
+import firebase from './firebase'
+import { getAuth } from 'firebase/auth';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmnk7rrwvf-qKaF621g-R--vIjZLBAjwo",
+  authDomain: "eventos-7afbc.firebaseapp.com",
+  projectId: "eventos-7afbc",
+  storageBucket: "eventos-7afbc.appspot.com",
+  messagingSenderId: "670401102223",
+  appId: "1:670401102223:web:60da82ad4a89689e92cbfa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+export default auth
+
--- site-eventos/src/view/login/index.js
+++ site-eventos/src/view/login/index.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../view/login/login.css'
+import  auth  from '../../config/firebase'
+
+import firebase from '../../config/firebase'
+import 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 function Login() {
+
+    const [registerEmail, setRegisterEmail] = useState()
+    const [registerSenha, setRegisterSenha] = useState()
+    const [email, setEmail] = useState()
+    const [senha, setSenha] = useState()
+
+
+    const register = async () => {
+        try {
+        const user = await createUserWithEmailAndPassword(auth, registerEmail, registerSenha)
+        } catch (erro) {
+            console.log(erro.message)
+        }
+    }
+
+    const logar = async () => {
+        
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                email, 
+                senha)
+                console.log('Login realizado!')
+            } catch (erro) {
+                console.log(erro.message)
+            }
+        
+    }
+
     return (
     <div className="login-content d-flex align-items-center">
         <form className="form-signin mx-auto">
             
-            <h1 class="h3 mb-3 fw-bold text-white font-weight-bold text-center">Login</h1>
+            <h1 className="h3 mb-3 fw-bold text-white font-weight-bold text-center">Login</h1>
 
             
-            <input type="email" id="inputEmail" class="form-control my-2" placeholder="email"/>
+            <input onChange={(e) => setEmail(e.target.value) }type="email" id="inputEmail" class="form-control my-2" placeholder="email"/>
 
-            <input type="password" id="inputPassword" class="form-control my-2" placeholder="senha"/>
+            <input onChange={(e) => setSenha(e.target.value) }type="password" id="inputPassword" className="form-control my-2" placeholder="senha"/>
 
             <div className="checkbox mb-3">
             <label>
                 <input type="checkbox" value="remember-me"/> Remember me
             </label>
             </div>
-            <button className="w-100 btn btn-lg btn-login" type="submit">Sign in</button>
+            <button onClick={logar} className="w-100 btn btn-lg btn-login" type="button">Login</button>
 
             <div className="msg-login text-white text-center my-4">
                 <span><strong>Foi!</strong> Voce esta conectado! &#129299;</span>
