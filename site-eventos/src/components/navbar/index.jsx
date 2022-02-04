7983ebb37a9de5ad34157f0fd3a0a262ed985a33
João Hélio <85315750+JoaoSantosJHX@users.noreply.github.com>
2022-02-04 09:42:56 -0300

    logout added

--- site-eventos/src/components/navbar/index.jsx
+++ site-eventos/src/components/navbar/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
+import { useSelector, useDispatch }  from 'react-redux'
+
 const Navbar = () => {
+
+    const dispatch = useDispatch()
     return (
         <nav className="navbar navbar-expand-lg ">
             <div className="container-fluid">
@@ -9,11 +13,24 @@ const Navbar = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-bars text-white"></i>
                      </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link> </li>
-            <li className="nav-item"><Link className="nav-link" to="novousuario">Cadastrar</Link> </li>
-            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link> </li>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+
+                    <li className="nav-item"><Link className="nav-link" to="/">Home</Link> </li>
+
+                    {    useSelector(state => state.usuarioLogado) > 0 ?
+                    <>
+                    <li className="nav-item"><Link className="nav-link" to="">Publicar Eventos</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="">Meus Eventos</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to='/login'onClick={() => dispatch({type: 'LOG_OUT', })}>Sair</Link> </li>
+                    </>
+                    :
+                    <>
+                    <li className="nav-item"><Link className="nav-link" to="novousuario">Cadastrar</Link> </li>
+                    <li className="nav-item"><Link className="nav-link" to="/login">Login</Link> </li>
+                    </>
+                    }
+                    
                     </ul>
                 </div>
             </div>
