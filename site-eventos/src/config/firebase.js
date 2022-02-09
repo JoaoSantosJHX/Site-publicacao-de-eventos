b877bfa3dee3145f1c56098365eb2291a93ad276
João Hélio <85315750+JoaoSantosJHX@users.noreply.github.com>
2022-02-09 20:52:53 -0300

    shit went wrong

--- site-eventos/src/config/firebase.js
+++ site-eventos/src/config/firebase.js
@@ -1,4 +1,5 @@
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -21,4 +22,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 
 export default auth
+
+export const storage = getStorage(app)
 
